1e816996bf0c450dad7f080a53880de4899bf16a
Manish-Hub <53503956+Manish-Hub@users.noreply.github.com>
2025-09-27 21:21:34 +0530

    Update train_model.py

--- pipeline/train_model.py
+++ pipeline/train_model.py
@@ -1,1 +1,10 @@
+from xgboost import XGBClassifier
+import joblib
 
+def train_model(X_train, y_train):
+    model = XGBClassifier()
+    model.fit(X_train, y_train)
+    return model
+
+def save_model(model, path):
+    joblib.dump(model, path)
